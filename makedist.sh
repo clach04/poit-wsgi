220bb8262c097b0c1adb7f22a88a632bd3f396f8
Yang Zhao <yang@yangman.ca>
2009-09-30 22:31:02 -0700

    Add script for making distribution tarballs

---
+++ makedist.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+version=`awk '/POIT_VERSION = ".*"/ {gsub(/"/, "", $3); print $3}' poit.py`
+d="poit-${version}"
+f="${d}.tar.bz2"
+
+echo Making $f
+if [[ ! -d dist ]]; then
+  mkdir dist || exit 1
+fi
+cd dist
+rm -rf $d
+mkdir $d
+cp ../poit.py ../poit.css ../poit.conf.example ../README $d
+
+tar -cjf $f $d
+md5sum $f > "${f}.md5"
+sha1sum $f > "${f}.sha1"
+
+rm -rf $d
+
+cat "${f}.md5"
+cat "${f}.sha1"
